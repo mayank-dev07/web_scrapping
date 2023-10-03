403ebca09f3138d8fbd196148fb21127f42fa9ed
mayank-dev07 <devmayank.hts@gmail.com>
2023-10-03 22:21:54 +0530

    commit

--- scrapping.py
+++ scrapping.py
@@ -9,14 +9,14 @@ soup = BeautifulSoup(page.text, "html.parser")
 articles = soup.find_all('article')
 
 for article in articles:
-    
+
     date = article.find('p', class_='text_wrapper__i87JK').get_text()
     title = article.find('a', class_='blog_title__eH3aB').get_text()
     content = article.find('div', class_="prose prose-vercel blog_prose__AcmB0")
     para = content.find('p').get_text()
 
     print("Date:" + date)
-    print("Title" + title)
+    print("Title: " + title)
     print(para)
 
     link = article.find('ul')
